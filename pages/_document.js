3b9ff7e6b3572f3ecf6016d7a636066cdaee04bf
MEHD-IPRO <mehdi.edarqaouy@gmail.com>
2023-09-21 13:19:51 -0700

    first ads

--- pages/_document.js
+++ pages/_document.js
@@ -30,30 +30,6 @@ class MyDocument extends Document {
 
           {/* Add the second provided script tag here */}
           <script type="text/javascript" src="//pl20691510.highcpmrevenuegate.com/c1/01/55/c101558b689533d487877dc298f2ec4a.js"></script>
-
-          {/* Add the third provided script tag here */}
-          <script type="text/javascript">
-            atOptions = {
-              'key' : '9c5952cc07ada187887cb16326cac1e8',
-              'format' : 'iframe',
-              'height' : 600,
-              'width' : 160,
-              'params' : {}
-            };
-            document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/9c5952cc07ada187887cb16326cac1e8/invoke.js"></scr' + 'ipt>');
-          </script>
-
-          {/* Add the fourth provided script tag here */}
-          <script type="text/javascript">
-            atOptions = {
-              'key' : 'deb194919e9b256d89abb7d5c7aa350a',
-              'format' : 'iframe',
-              'height' : 250,
-              'width' : 300,
-              'params' : {}
-            };
-            document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/deb194919e9b256d89abb7d5c7aa350a/invoke.js"></scr' + 'ipt>');
-          </script>
         </Head>
         <body>
           <Main />

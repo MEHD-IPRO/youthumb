87b5384988c30e93461fc95e844ac628c03bc8ea
MEHD-IPRO <mehdi.edarqaouy@gmail.com>
2023-09-21 13:00:35 -0700

    first main

--- pages/_document.js
+++ pages/_document.js
@@ -23,22 +23,6 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-
-          {/* Add the new <script> tag here */}
-          <script
-            src="https://alwingulla.com/88/tag.min.js"
-            data-zone="11318"
-            async
-            data-cfasync="false"
-          ></script>
-
-          {/* Add the provided code snippet here */}
-          <script async type="text/javascript" src="//pl20691510.highcpmrevenuegate.com/c1/01/55/c101558b689533d487877dc298f2ec4a.js"></script>
-
-          {/* Add the previous code snippet */}
-          <script async="async" data-cfasync="false" src="//pl20691408.highcpmrevenuegate.com/f37be7919807e7658d1ecac4cbb7f670/invoke.js"></script>
-          <div id="container-f37be7919807e7658d1ecac4cbb7f670"></div>
-
         </Head>
         <body>
           <Main />

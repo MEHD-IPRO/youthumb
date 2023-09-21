9b3e7b388c4a9df387456fee23f2d42e986e6734
MEHD-IPRO <mehdi.edarqaouy@gmail.com>
2023-09-21 13:13:49 -0700

    new adsterra

--- pages/_document.js
+++ pages/_document.js
@@ -23,6 +23,37 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
+
+          {/* Add the first provided script tag here */}
+          <script async="async" data-cfasync="false" src="//pl20691408.highcpmrevenuegate.com/f37be7919807e7658d1ecac4cbb7f670/invoke.js"></script>
+          <div id="container-f37be7919807e7658d1ecac4cbb7f670"></div>
+
+          {/* Add the second provided script tag here */}
+          <script type="text/javascript" src="//pl20691510.highcpmrevenuegate.com/c1/01/55/c101558b689533d487877dc298f2ec4a.js"></script>
+
+          {/* Add the third provided script tag here */}
+          <script type="text/javascript">
+            atOptions = {
+              'key' : '9c5952cc07ada187887cb16326cac1e8',
+              'format' : 'iframe',
+              'height' : 600,
+              'width' : 160,
+              'params' : {}
+            };
+            document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/9c5952cc07ada187887cb16326cac1e8/invoke.js"></scr' + 'ipt>');
+          </script>
+
+          {/* Add the fourth provided script tag here */}
+          <script type="text/javascript">
+            atOptions = {
+              'key' : 'deb194919e9b256d89abb7d5c7aa350a',
+              'format' : 'iframe',
+              'height' : 250,
+              'width' : 300,
+              'params' : {}
+            };
+            document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/deb194919e9b256d89abb7d5c7aa350a/invoke.js"></scr' + 'ipt>');
+          </script>
         </Head>
         <body>
           <Main />

0d48bc8ab6c25024da4948f11b11d4111c028c51
MEHD-IPRO <mehdi.edarqaouy@gmail.com>
2023-09-22 14:16:09 -0700

    new ban

--- pages/_document.js
+++ pages/_document.js
@@ -32,21 +32,16 @@ class MyDocument extends Document {
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
 
-          {/* Add the provided script tag here */}
+          {/* Add the provided script tag */}
           <script async="async" data-cfasync="false" src="//pl20691408.highcpmrevenuegate.com/f37be7919807e7658d1ecac4cbb7f670/invoke.js"></script>
           <div id="container-f37be7919807e7658d1ecac4cbb7f670"></div>
 
-          {/* Add the second provided script tag here */}
-          <script type="text/javascript" src="//pl20691510.highcpmrevenuegate.com/c1/01/55/c101558b689533d487877dc298f2ec4a.js"></script>
-
-          {/* Add the third provided script tag here */}
-          <script type="text/javascript" src="//pl20691510.highcpmrevenuegate.com/c1/01/55/c101558b689533d487877dc298f2ec4a.js"></script>
-
-          {/* Add the fourth provided script tag here */}
-          <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
+          {/* Rest of your script tags */}
+          {/* <script type="text/javascript" src="//pl20691510.highcpmrevenuegate.com/c1/01/55/c101558b689533d487877dc298f2ec4a.js"></script> */}
+          {/* <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
             var atOptions = ${JSON.stringify(atOptions)};
             document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/deb194919e9b256d89abb7d5c7aa350a/invoke.js"></scr' + 'ipt>');
-          ` }} />
+          ` }} /> */}
         </Head>
         <body>
           <Main />

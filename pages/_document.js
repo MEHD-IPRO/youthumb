aa59d3744943129d2f9ca84c8462092374b7b75c
MEHD-IPRO <mehdi.edarqaouy@gmail.com>
2023-09-23 10:07:40 -0700

    way

--- pages/_document.js
+++ pages/_document.js
@@ -42,9 +42,6 @@ class MyDocument extends Document {
           {/* Add the second provided script tag here */}
           <script type="text/javascript" src="//pl20691510.highcpmrevenuegate.com/c1/01/55/c101558b689533d487877dc298f2ec4a.js"></script>
 
-          {/* Add the third provided script tag here */}
-          <script type="text/javascript" src="//pl20691510.highcpmrevenuegate.com/c1/01/55/c101558b689533d487877dc298f2ec4a.js"></script>
-
           {/* Add the fourth provided script tag here */}
           <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
             var atOptions = ${JSON.stringify(atOptions)};

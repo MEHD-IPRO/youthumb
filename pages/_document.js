38dd20403baf5374ee1bde46fbb2221c58e49abc
MEHD-IPRO <mehdi.edarqaouy@gmail.com>
2023-09-23 05:00:11 -0700

    fix new

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,14 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
+    const atOptions = {
+      key: 'deb194919e9b256d89abb7d5c7aa350a',
+      format: 'iframe',
+      height: 250,
+      width: 300,
+      params: {}
+    };
+
     return (
       <Html lang="en">
         <Head>
@@ -24,34 +32,21 @@ class MyDocument extends Document {
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
 
-          {/* Add the provided script tag */}
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `
-                var atOptions = {
-                  key: 'deb194919e9b256d89abb7d5c7aa350a',
-                  format: 'iframe',
-                  height: 250,
-                  width: 300,
-                  params: {}
-                };
-                document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/2288353876086512976a0a6367405128/invoke.js"></scr' + 'ipt>');
-              `,
-            }}
-          />
+          {/* Add the first provided script tag here */}
+          <script async="async" data-cfasync="false" src="//pl20691408.highcpmrevenuegate.com/f37be7919807e7658d1ecac4cbb7f670/invoke.js"></script>
+          <div id="container-f37be7919807e7658d1ecac4cbb7f670"></div>
 
-          {/* Rest of your script tags */}
-          {/* <script type="text/javascript" src="//pl20691510.highcpmrevenuegate.com/c1/01/55/c101558b689533d487877dc298f2ec4a.js"></script> */}
-          {/* <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `
-                var atOptions = ${JSON.stringify(atOptions)};
-                document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/deb194919e9b256d89abb7d5c7aa350a/invoke.js"></scr' + 'ipt>');
-              `,
-            }}
-          /> */}
+          {/* Add the second provided script tag here */}
+          <script type="text/javascript" src="//pl20691510.highcpmrevenuegate.com/c1/01/55/c101558b689533d487877dc298f2ec4a.js"></script>
+
+          {/* Add the third provided script tag here */}
+          <script type="text/javascript" src="//pl20691510.highcpmrevenuegate.com/c1/01/55/c101558b689533d487877dc298f2ec4a.js"></script>
+
+          {/* Add the fourth provided script tag here */}
+          <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
+            var atOptions = ${JSON.stringify(atOptions)};
+            document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/deb194919e9b256d89abb7d5c7aa350a/invoke.js"></scr' + 'ipt>');
+          ` }} />
         </Head>
         <body>
           <Main />

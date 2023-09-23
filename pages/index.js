45afc029172d56b2240eb14579b3cc4705f5c68e
MEHD-IPRO <mehdi.edarqaouy@gmail.com>
2023-09-23 05:15:07 -0700

    old

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,83 @@
-// Initialize variables
-let videoURL = "";
-let thumbnailOptions = [];
+import { useState } from "react";
+import copy from "copy-to-clipboard";
 
-// Function to extract video ID from YouTube URL
-function getYouTubeVideoId(url) {
-  let match = url.match(/.(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]).*/);
-  return match && match[1].length === 11 ? match[1] : null;
-}
+const Index = () => {
+  const [videoURL, setVideoURL] = useState("");
+  const [thumbnailOptions, setThumbnailOptions] = useState([]);
 
-// Function to fetch thumbnail options
-function getYouTubeThumbnail(url) {
-  const videoId = getYouTubeVideoId(url);
-  if (videoId) {
-    const thumbnailBaseUrl = "http://img.youtube.com/vi/";
-    const options = [
-      { resolution: "HD (1280x720)", code: "maxresdefault" },
-      { resolution: "SD (640x480)", code: "sddefault" },
-      { resolution: "Normal (480x360)", code: "hqdefault" },
-      { resolution: "Medium (320x180)", code: "mqdefault" },
-      { resolution: "Low (120x90)", code: "default" },
-    ];
+  const getYouTubeThumbnail = (url) => {
+    let regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
+    let match = url.match(regExp);
 
-    thumbnailOptions = options.map((option) => ({
-      resolution: option.resolution,
-      url: `${thumbnailBaseUrl}${videoId}/${option.code}.jpg`,
-    }));
-  } else {
-    thumbnailOptions = [];
-  }
-}
+    if (match && match[1].length === 11) {
+      const videoURL = match[1];
+      const thumbnailBaseUrl = "http://img.youtube.com/vi/";
 
-// Function to download an image
-function downloadImage(url) {
-  const link = document.createElement("a");
-  link.href = url;
-  link.download = "thumbnail.jpg";
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
+      const options = [
+        { resolution: "HD (1280x720)", code: "maxresdefault" },
+        { resolution: "SD (640x480)", code: "sddefault" },
+        { resolution: "Normal (480x360)", code: "hqdefault" },
+        { resolution: "Medium (320x180)", code: "mqdefault" },
+        { resolution: "Low (120x90)", code: "default" },
+      ];
 
-// Event listener for clicking the "Download Thumbnails" button
-document.querySelector(".btn-blue").addEventListener("click", () => {
-  const input = document.querySelector("input");
-  videoURL = input.value;
-  getYouTubeThumbnail(videoURL);
-});
+      const thumbnailOptions = options.map((option) => ({
+        resolution: option.resolution,
+        url: `${thumbnailBaseUrl}${videoURL}/${option.code}.jpg`,
+      }));
 
-// Event listeners for clicking the "Download Image" buttons
-const thumbnailButtons = document.querySelectorAll(".btn-blue");
-thumbnailButtons.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    const imageUrl = thumbnailOptions[index].url;
-    downloadImage(imageUrl);
-  });
-});
+      setThumbnailOptions(thumbnailOptions);
+      setVideoURL("");
+    } else {
+      setThumbnailOptions([]);
+    }
+  };
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <header className="text-center mb-8">
+        <h1 className="text-3xl font-bold mb-2">
+          Youtube Thumbnail Downloader
+        </h1>
+        <p className="text-gray-600">
+          Download high-quality thumbnails from YouTube videos.
+        </p>
+      </header>
+      <div className="text-center">
+        <input
+          type="text"
+          className="w-full md:w-1/2 px-4 py-2 border rounded"
+          placeholder="Enter YouTube URL"
+          value={videoURL}
+          onChange={(e) => setVideoURL(e.target.value)}
+        />
+        <button
+          className="btn-blue mt-2"
+          onClick={() => getYouTubeThumbnail(videoURL)}
+        >
+          Download Thumbnails
+        </button>
+      </div>
+      {thumbnailOptions.length > 0 && (
+        <div className="mt-8">
+          <h2 className="text-xl font-semibold mb-4">Thumbnail Options</h2>
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {thumbnailOptions.map((option, index) => (
+              <div key={index} className="thumbnail-option">
+                <img src={option.url} alt={`Thumbnail ${index + 1}`} />
+                <button
+                  className="btn-blue mt-2"
+                  onClick={() => copy(option.url)}
+                >
+                  Copy Image URL
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Index;

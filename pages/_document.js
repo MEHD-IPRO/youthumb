5822cfbd9e3f604316730e213101ddc267419a46
MEHD-IPRO <mehdi.edarqaouy@gmail.com>
2023-09-30 04:21:50 -0700

    NATIV

--- pages/_document.js
+++ pages/_document.js
@@ -37,42 +37,10 @@ class MyDocument extends Document {
 
           {/* RevenueGate script */}
           <script type='text/javascript' src='//pl20691510.highcpmrevenuegate.com/c1/01/55/c101558b689533d487877dc298f2ec4a.js'></script>
-
-          {/* Additional script 1 */}
-          <script type="text/javascript">
-            {`
-              atOptions = {
-                'key' : '9c5952cc07ada187887cb16326cac1e8',
-                'format' : 'iframe',
-                'height' : 600,
-                'width' : 160,
-                'params' : {}
-              };
-              document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/9c5952cc07ada187887cb16326cac1e8/invoke.js"></scr' + 'ipt>');
-            `}
-          </script>
-
-          {/* Additional script 2 */}
-          <script type="text/javascript">
-            {`
-              atOptions = {
-                'key' : '2288353876086512976a0a6367405128',
-                'format' : 'iframe',
-                'height' : 60,
-                'width' : 468,
-                'params' : {}
-              };
-              document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/2288353876086512976a0a6367405128/invoke.js"></scr' + 'ipt>');
-            `}
-          </script>
         </Head>
         <body>
           <Main />
           <NextScript />
-          {/* Centered Banner */}
-          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <a href=""><img alt="banner" src="https://landings-cdn.adsterratech.com/referralBanners/gif/700x90_adsterra_reff.gif" /></a>
-          </div>
         </body>
       </Html>
     );
